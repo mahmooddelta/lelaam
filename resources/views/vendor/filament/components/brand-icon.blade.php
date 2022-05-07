52bdf0fd83dc993ec8552a32a55671b5c569e45d
Ali Gulzar <ali.gulzar729@gmail.com>
2022-05-07 15:23:57 +0430

    Update brand logo

--- resources/views/vendor/filament/components/brand-icon.blade.php
+++ resources/views/vendor/filament/components/brand-icon.blade.php
@@ -3,14 +3,6 @@
         'text-xl font-bold tracking-tight filament-brand',
         'dark:text-white' => config('filament.dark_mode'),
     ])>
-        {{
-            \Illuminate\Support\Str::of($brand)
-                ->snake()
-                ->upper()
-                ->explode('_')
-                ->map(fn (string $string) => \Illuminate\Support\Str::substr($string, 0, 1))
-                ->take(2)
-                ->implode('')
-        }}
+        <img src="{{ secure_asset('images/logo.png') }}" alt="Icon" class="h-full w-full object-contain" />
     </div>
 @endif
--- resources/views/vendor/filament/components/brand.blade.php
+++ resources/views/vendor/filament/components/brand.blade.php
@@ -1,8 +1,11 @@
 @if (filled($brand = config('filament.brand')))
     <div @class([
-        'text-xl font-bold tracking-tight filament-brand',
+        'text-xl font-bold tracking-tight filament-brand flex items-center justify-center content-center',
         'dark:text-white' => config('filament.dark_mode'),
     ])>
-        {{ $brand }}
+        <div>
+            <img src="{{ secure_asset('images/logo.png') }}" alt="{{ config('app.name') }} logo"
+                 style="height: 90px; margin-right: 100%;">
+        </div>
     </div>
 @endif
