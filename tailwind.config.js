16e7160de4eba4c8d2a591a2805bd11b9bd71b12
Ali Gulzar <ali.gulzar729@gmail.com>
2022-05-10 10:53:17 +0430

    Added and configured front-end libraries and themes
    Daisy UI
    Theme changer
    Tailwind flip for automatic RTL classes conversion.

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                sans: ['Vazirmatn', ...defaultTheme.fontFamily.sans],
             },
             colors: {
                 primary: colors.rose,
@@ -22,8 +22,43 @@ module.exports = {
                 success: colors.green,
                 warning: colors.yellow,
             },
+
         },
     },
+    daisyui: {
+        themes: [
+            {
 
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+                dracula: {
+                    ...require("daisyui/src/colors/themes")["[data-theme=dracula]"],
+                    primary: "#EE3E43",
+                    secondary: "#F3F4F6",
+                    accent: "#8b5cf6",
+                    info: "#3ABFF8",
+                    success: "#22BE00",
+                    warning: "#FBBD23",
+                    error: "#F87272",
+                },
+                pastel: {
+                    ...require("daisyui/src/colors/themes")["[data-theme=pastel]"],
+                    primary: "#EE3E43",
+                    secondary: "#F3F4F6",
+                    accent: "#8b5cf6",
+                    info: "#3ABFF8",
+                    success: "#22BE00",
+                    warning: "#FBBD23",
+                    error: "#F87272",
+                },
+            },
+        ],
+        rtl: true,
+        darkTheme: "dracula"
+    },
+
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+        require("daisyui"),
+        require("tailwindcss-flip"),
+    ],
 };
