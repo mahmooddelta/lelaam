11de13a9794a905a79f93f705efbe0910d1e4cea
Ali Gulzar <ali.gulzar729@gmail.com>
2022-05-11 14:38:24 +0430

    Add slug macro to Slug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Filament\Facades\Filament;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider {
 	
@@ -24,6 +25,15 @@ class AppServiceProvider extends ServiceProvider {
 	public function boot () {
 		Filament::serving(function () {
 			Filament::registerTheme(mix('css/app.css'));
+		});
+		
+		Str::macro('persian_slug', function ($string, $separator = '-') {
+			$string = trim($string);
+			$string = mb_strtolower($string, 'UTF-8');
+			$string = preg_replace("/[^a-z0-9_\-\sءاآؤئبپتثجچحخدذرزژسشصضطظعغفقكکگلمنوهی]/u", '', $string);
+			$string = preg_replace("/[\s\-_]+/", ' ', $string);
+			
+			return preg_replace("/[\s_]/", $separator, $string);
 		});
 	}
 }
