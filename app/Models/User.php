841fc012bb44c66c44b4391c058d6841782a012e
Ali Gulzar <ali.gulzar729@gmail.com>
2022-05-14 13:15:37 +0430

    Add ads relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use BezhanSalleh\FilamentShield\Traits\HasFilamentShield;
 use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
@@ -14,53 +15,59 @@ use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable implements HasMedia, FilamentUser {
-	
-	use HasApiTokens;
-	use HasFactory;
-	use HasProfilePhoto;
-	use Notifiable;
-	use TwoFactorAuthenticatable;
-	use HasRoles, HasFilamentShield, InteractsWithMedia;
-	
-	/**
-	 * The attributes that are mass assignable.
-	 *
-	 * @var string[]
-	 */
-	protected $fillable = [
-		'name',
-		'email',
-		'password',
-	];
-	
-	/**
-	 * The attributes that should be hidden for serialization.
-	 *
-	 * @var array
-	 */
-	protected $hidden = [
-		'password',
-		'remember_token',
-		'two_factor_recovery_codes',
-		'two_factor_secret',
-	];
-	
-	/**
-	 * The attributes that should be cast.
-	 *
-	 * @var array
-	 */
-	protected $casts = [
-		'email_verified_at' => 'datetime',
-	];
-	
-	/**
-	 * The accessors to append to the model's array form.
-	 *
-	 * @var array
-	 */
-	protected $appends = [
-		'profile_photo_url',
-	];
+class User extends Authenticatable implements HasMedia, FilamentUser
+{
+
+    use HasApiTokens;
+    use HasFactory;
+    use HasProfilePhoto;
+    use Notifiable;
+    use TwoFactorAuthenticatable;
+    use HasRoles, HasFilamentShield, InteractsWithMedia;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'two_factor_recovery_codes',
+        'two_factor_secret',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = [
+        'profile_photo_url',
+    ];
+
+    public function ads(): HasMany
+    {
+        return $this->hasMany(Ad::class);
+    }
 }
