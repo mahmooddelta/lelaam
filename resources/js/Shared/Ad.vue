51003128675d42ee2ee45fb41fb056c3edb07e67
Ali Gulzar <ali.gulzar729@gmail.com>
2022-05-18 13:24:56 +0430

    Make Category and Ad independent component

---
+++ resources/js/Shared/Ad.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+    ad: Object,
+})
+</script>
+
+<template>
+    <Link class="card card-side bg-base-100 shadow-xl my-2 text-right" :href="route('ad.show', { ad: ad.slug })">
+        <figure><img :src="ad.thumb" :alt="`${ad.name} image`" class="w-32 object-contain"></figure>
+        <div class="card-body">
+            <h2 class="font-bold text-primary text-xl" v-html="ad.title"></h2>
+            <p v-html="`<b>${ad.price}</b> ${ad.currency}`"></p>
+            <p v-html="ad.created_at + ' در ' + ad.district "></p>
+        </div>
+    </Link>
+</template>
---
+++ resources/js/Shared/Category.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+    category: Object,
+})
+</script>
+
+<template>
+    <div class="w-full card card-side bg-base-300 shadow-xl my-2">
+        <div class="card-body text-center">
+            <h2 class="font-bold text-lg" v-html="category.name"></h2>
+            <p class="text-sm" v-text="category.ads_count + ' آگهی '"></p>
+            <div class="card-actions justify-center my-2">
+                <Link :href="route('ads', { category: category.slug })" class="btn btn-primary btn-sm">دیدن تمام آگهی ها</Link>
+            </div>
+        </div>
+    </div>
+</template>
