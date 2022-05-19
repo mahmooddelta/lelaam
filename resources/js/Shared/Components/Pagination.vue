9ea1c52033c0b5f49ae3256f8eb146f3aac53b39
Ali Gulzar <ali.gulzar729@gmail.com>
2022-05-19 14:43:14 +0430

    Implement Pagination Component
    But the trick is that you must pass `meta.links` as links prop and the data must be passed to view from controller while wrapped in API Resource.

---
+++ resources/js/Shared/Components/Pagination.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+    links: Array,
+})
+</script>
+
+<template>
+    <div v-if="links.length > 3">
+        <div class="flex flex-wrap justify-center -mb-1">
+            <template v-for="(link, key) in links">
+                <div v-if="link.url === null" :key="key" class="mb-1 mr-1 px-4 py-3 text-gray-400 text-sm btn btn-disabled" v-html="link.label"/>
+                <Link v-else :key="`link-${key}`"
+                      class="mb-1 mr-1 px-4 py-3 text-sm btn btn-outline"
+                      :class="{ 'btn-primary': link.active }" :href="link.url" v-html="link.label"/>
+            </template>
+        </div>
+    </div>
+</template>
