d9f99f873268bdc30382a6352f5333fcd753919a
Ali Gulzar <ali.gulzar729@gmail.com>
2022-05-19 09:23:52 +0430

    Add Account Links

--- resources/js/Shared/TopNavLinks.vue
+++ resources/js/Shared/TopNavLinks.vue
@@ -13,11 +13,16 @@
             </li>
         </ul>
         <ul class="menu menu-horizontal">
-            <li>
-                <Link :href="route('chat')">چت</Link>
+<!--            <li>-->
+<!--                <Link :href="route('chat')">چت</Link>-->
+<!--            </li>-->
+            <!-- Account -->
+            <li v-if="$page.props.user">
+                <Link :href="route('account')">حساب من</Link>
             </li>
             <li>
-                <Link :href="route('account')">حساب من</Link>
+                <Link :href="route('register')">ثبت نام</Link>
+                <Link :href="route('login')">ورود</Link>
             </li>
         </ul>
     </section>
