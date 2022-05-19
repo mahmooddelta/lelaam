54b3966f3f7756a0275b273319182d48ff3ad215
Ali Gulzar <ali.gulzar729@gmail.com>
2022-05-19 14:50:14 +0430

    Fix undefined when there is no ads for that category

--- resources/js/Shared/Category.vue
+++ resources/js/Shared/Category.vue
@@ -8,7 +8,7 @@ defineProps({
     <div class="w-full card card-side bg-base-300 shadow-xl my-2">
         <div class="card-body text-center">
             <h2 class="font-bold text-lg" v-html="category.name"></h2>
-            <p class="text-sm" v-text="category.ads_count + ' آگهی '"></p>
+            <p class="text-sm" v-text="`${(category.ads_count || 0)} آگهی `"></p>
             <div class="card-actions justify-center my-2">
                 <Link :href="route('ads', { category: category.slug })" class="btn btn-primary btn-sm">دیدن تمام آگهی ها</Link>
             </div>
