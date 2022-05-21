409ccfeb04c03ee97637c4f79702df3bea4b8ba3
Ali Gulzar <ali.gulzar729@gmail.com>
2022-05-21 12:32:27 +0430

    Fix class typo

--- resources/js/Shared/Filters.vue
+++ resources/js/Shared/Filters.vue
@@ -33,7 +33,7 @@ watch(filters.state, (value) => {
     <form class="p-4 rounded-lg">
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-4">
             <input
-                class="input input-bordered input-transparent"
+                class="input input-bordered bg-transparent"
                 v-model="filters.search"
                 placeholder="جستجو..."
             />
