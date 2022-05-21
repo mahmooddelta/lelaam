79386ca0641ae441e9f5d6a20fdb3f627f6ddc5c
Ali Gulzar <ali.gulzar729@gmail.com>
2022-05-21 12:36:49 +0430

    Fix padding must be used instead of margin

--- resources/js/Pages/Categories.vue
+++ resources/js/Pages/Categories.vue
@@ -9,7 +9,7 @@ defineProps({
 
 <template>
     <Head title="دسته بندی ها"/>
-    <h1 class="text-5xl text-center text-primary-500 underline my-4">دسته بندی ها</h1>
+    <h1 class="text-5xl text-center text-primary-500 underline py-4">دسته بندی ها</h1>
     <section class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xs:grid-cols-4 gap-4 px-10">
         <Category v-for="category in categories.data" :key="category.id" :category="category"/>
     </section>
