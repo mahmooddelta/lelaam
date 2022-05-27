a467bdd19d46437e8a1710e8ae1866d6fe579a18
Ali Gulzar <ali.gulzar729@gmail.com>
2022-05-27 18:47:00 +0430

    Add Preview Conversion for MLP

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,35 +5,45 @@ namespace App\Providers;
 use Filament\Facades\Filament;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
+use Spatie\Image\Manipulations;
+use Spatie\MediaLibrary\Conversions\Conversion;
+use Spatie\MediaLibraryPro\Models\TemporaryUpload;
 
-class AppServiceProvider extends ServiceProvider {
-	
-	/**
-	 * Register any application services.
-	 *
-	 * @return void
-	 */
-	public function register () {
-		//
-	}
-	
-	/**
-	 * Bootstrap any application services.
-	 *
-	 * @return void
-	 */
-	public function boot () {
-		Filament::serving(function () {
-			Filament::registerTheme(mix('css/app.css'));
-		});
-		
-		Str::macro('persian_slug', function ($string, $separator = '-') {
-			$string = trim($string);
-			$string = mb_strtolower($string, 'UTF-8');
-			$string = preg_replace("/[^a-z0-9_\-\sءاآؤئبپتثجچحخدذرزژسشصضطظعغفقكکگلمنوهی]/u", '', $string);
-			$string = preg_replace("/[\s\-_]+/", ' ', $string);
-			
-			return preg_replace("/[\s_]/", $separator, $string);
-		});
-	}
+class AppServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Filament::serving(function () {
+            Filament::registerTheme(mix('css/app.css'));
+        });
+
+        Str::macro('persian_slug', function ($string, $separator = '-') {
+            $string = trim($string);
+            $string = mb_strtolower($string, 'UTF-8');
+            $string = preg_replace("/[^a-z0-9_\-\sءاآؤئبپتثجچحخدذرزژسشصضطظعغفقكکگلمنوهی]/u", '', $string);
+            $string = preg_replace("/[\s\-_]+/", ' ', $string);
+
+            return preg_replace("/[\s_]/", $separator, $string);
+        });
+
+        TemporaryUpload::previewManipulation(function (Conversion $conversion) {
+            $conversion->fit(Manipulations::FIT_CROP, 300, 300);
+        });
+    }
 }
