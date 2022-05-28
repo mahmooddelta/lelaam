b42e76b0641dbb960606bd1665b3f2cb342ce66f
Ali Gulzar <ali.gulzar729@gmail.com>
2022-05-28 13:32:27 +0430

    Improved UI

--- resources/js/Shared/TopNavLinks.vue
+++ resources/js/Shared/TopNavLinks.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <section class="flex align-between justify-between px-4 bg-base-100">
+    <section class="flex align-between justify-between px-4 bg-base-100 text-xs">
         <ul class="menu menu-horizontal">
             <li>
                 <Link :href="route('ads')">آگهی ها</Link>
@@ -20,10 +20,10 @@
             <li v-if="$page.props.user">
                 <Link :href="route('account')">حساب من</Link>
                 <Link :href="route('logout')" as="form" method="post">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
                     </svg>
-                    خروج از حساب کاربری
+                    خروج
                 </Link>
             </li>
             <li v-else>
