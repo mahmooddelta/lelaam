4fd5fd0996094e5adcab1414864c8b1d8542da1b
Ali Gulzar <ali.gulzar729@gmail.com>
2022-05-29 14:23:46 +0430

    Add children relationship to be included in response

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -19,6 +19,7 @@ class CategoryResource extends JsonResource
             'name' => $this->name,
             'slug' => $this->slug,
             'description' => $this->when($this->description, $this->description),
+            'children' => $this->whenLoaded('children', self::collection($this->children)),
 
             'ads_count' => $this->when($this->ads_count, $this->ads_count),
             'attributes_count' => $this->when($this->attributes_count, $this->attributes_count),
