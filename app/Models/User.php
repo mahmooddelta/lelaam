be19da226fe37d1f57242758d35dbbcd661562df
Ali Gulzar <ali.gulzar729@gmail.com>
2022-05-31 13:58:15 +0430

    Hash password automatically

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
@@ -93,5 +94,10 @@ class User extends Authenticatable implements HasMedia, FilamentUser, JWTSubject
     public function getJWTCustomClaims(): array
     {
         return [];
+    }
+
+    public function setPasswordAttribute($value): string
+    {
+        return Hash::make($value);
     }
 }
