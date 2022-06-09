dd307d2ae5e1cfb0e4c7b35f07428e78de969f6b
Ali Gulzar <ali.gulzar729@gmail.com>
2022-06-09 13:28:33 +0430

    Add background to make it look more refined

--- resources/js/Shared/Components/Collapse.vue
+++ resources/js/Shared/Components/Collapse.vue
@@ -8,7 +8,7 @@ defineProps({
 </script>
 
 <template>
-    <div tabindex="0" class="collapse collapse-arrow border-2 border-primary-300 rounded-box my-2">
+    <div tabindex="0" class="collapse collapse-arrow border-2 border-primary-300 rounded-box my-2 bg-base-300">
         <input type="checkbox"/>
         <div class="collapse-title text-xl font-medium" v-text="title"></div>
         <div class="collapse-content">
