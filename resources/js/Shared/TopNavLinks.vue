eb704690c7578461e59ac57324f839e86fd37e52
Ali Gulzar <ali.gulzar729@gmail.com>
2022-06-15 08:54:36 +0430

    Add Chats link to Navbar
    Move logout to account page

--- resources/js/Shared/TopNavLinks.vue
+++ resources/js/Shared/TopNavLinks.vue
@@ -13,18 +13,12 @@
             </li>
         </ul>
         <ul class="menu menu-horizontal">
-            <!--            <li>-->
-            <!--                <Link :href="route('chat')">چت</Link>-->
-            <!--            </li>-->
+            <li>
+                <Link :href="route('chat')">گفتگو ها</Link>
+            </li>
             <!-- Account -->
             <li v-if="$page.props.user">
                 <Link :href="route('account')">حساب من</Link>
-                <Link :href="route('logout')" as="form" method="post">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
-                    </svg>
-                    خروج
-                </Link>
             </li>
             <li v-else>
                 <Link :href="route('register')">ثبت نام</Link>
