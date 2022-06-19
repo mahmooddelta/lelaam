d1143b86dbe209c04835959f83b9bcd17b7d45d8
Ali Gulzar <ali.gulzar729@gmail.com>
2022-06-19 19:26:27 +0430

    Update response status code

--- app/Http/Middleware/JWT.php
+++ app/Http/Middleware/JWT.php
@@ -9,6 +9,7 @@ use PHPOpenSourceSaver\JWTAuth\Exceptions\TokenExpiredException;
 use PHPOpenSourceSaver\JWTAuth\Exceptions\TokenInvalidException;
 use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
 use PHPOpenSourceSaver\JWTAuth\Http\Middleware\BaseMiddleware;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 use function response;
 
 class JWT extends BaseMiddleware
@@ -19,14 +20,14 @@ class JWT extends BaseMiddleware
             JWTAuth::parseToken()->authenticate();
         } catch (Exception $e) {
             if ($e instanceof TokenInvalidException) {
-                return response()->json(['status' => '!توکن ورود درست نیست']);
+                return response()->json(['status' => '!توکن ورود درست نیست', ResponseAlias::HTTP_UNAUTHORIZED]);
             }
 
             if ($e instanceof TokenExpiredException) {
-                return response()->json(['status' => 'توکن منقضی شده است!']);
+                return response()->json(['status' => 'توکن منقضی شده است!', ResponseAlias::HTTP_UNAUTHORIZED]);
             }
 
-            return response()->json(['status' => '!توکن ورود درست نیست']);
+            return response()->json(['status' => '!توکن ورود درست نیست', ResponseAlias::HTTP_UNAUTHORIZED]);
         }
 
         return $next($request);
