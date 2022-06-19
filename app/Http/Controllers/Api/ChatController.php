3f997bb1042f55d330e366895b0a569c7c2a5b13
Ali Gulzar <ali.gulzar729@gmail.com>
2022-06-19 11:33:55 +0430

    Broadcast and Store Message in a transaction

--- app/Http/Controllers/Api/ChatController.php
+++ app/Http/Controllers/Api/ChatController.php
@@ -14,6 +14,7 @@ use Exception;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class ChatController extends Controller
@@ -55,13 +56,15 @@ class ChatController extends Controller
     {
         $ad->load(['media'])->select(['id', 'title', 'slug', 'phone_number', 'user_id']);
         try {
-            $message = Message::create([
-                'ad_id' => $ad->value('id'),
-                'sender_id' => auth()->id(),
-                'receiver_id' => $ad->value('user_id'),
-                'body' => $request->validated('message'),
-            ]);
-            broadcast(new MessageSent(UserResource::make(auth()->user()), MessageResource::make($message), AdResource::make($ad)))->toOthers();
+            DB::transaction(function () use ($request, $ad) {
+                $message = Message::create([
+                    'ad_id' => $ad->value('id'),
+                    'sender_id' => auth()->id(),
+                    'receiver_id' => $ad->value('user_id'),
+                    'body' => $request->validated('message'),
+                ]);
+                broadcast(new MessageSent(UserResource::make(auth()->user()), MessageResource::make($message), AdResource::make($ad)))->toOthers();
+            });
         } catch (Exception $exception) {
             Log::error($exception);
             return \response()->json(['message' => 'مشکلی در ارسال پیام شما پیش آمده است. لطفا دوباره کوشش کنید!']);
--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -3,14 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Events\MessageSent;
+use App\Http\Requests\Chat\StoreRequest;
 use App\Http\Resources\AdResource;
 use App\Http\Resources\MessageResource;
 use App\Http\Resources\UserResource;
 use App\Models\Ad;
 use App\Models\Message;
+use Exception;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -53,16 +57,23 @@ class ChatController extends Controller
         ]);
     }
 
-    public function store(Request $request): RedirectResponse
+    public function store(StoreRequest $request): RedirectResponse
     {
         $ad = Ad::whereSlug($request->post)->with(['media'])->select(['id', 'title', 'slug', 'phone_number', 'user_id']);
-        $message = Message::create([
-            'ad_id' => $ad->value('id'),
-            'sender_id' => auth()->id(),
-            'receiver_id' => $ad->value('user_id'),
-            'body' => $request->message,
-        ]);
-        broadcast(new MessageSent(UserResource::make(auth()->user()), MessageResource::make($message), AdResource::make($ad->first())))->toOthers();
-        return back()->with(['message' => 'پیام ارسال شد.']);
+        try {
+            DB::transaction(function () use ($request, $ad) {
+                $message = Message::create([
+                    'ad_id' => $ad->value('id'),
+                    'sender_id' => auth()->id(),
+                    'receiver_id' => $ad->value('user_id'),
+                    'body' => $request->validated('message'),
+                ]);
+                broadcast(new MessageSent(UserResource::make(auth()->user()), MessageResource::make($message), AdResource::make($ad->first())))->toOthers();
+            });
+        } catch (Exception $exception) {
+            Log::error($exception);
+            return back()->json(['message' => 'مشکلی در ارسال پیام شما پیش آمده است. لطفا دوباره کوشش کنید!']);
+        }
+        return back()->json(['message' => 'پیام ارسال شد.']);
     }
 }
