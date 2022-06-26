f33e2c68c183244f32f5d95fb75605f9107ca04a
Ali Gulzar <ali.gulzar729@gmail.com>
2022-06-26 08:39:39 +0430

    Make the layout width bigger and add Alert

--- resources/js/Jetstream/AuthenticationCard.vue
+++ resources/js/Jetstream/AuthenticationCard.vue
@@ -1,11 +1,18 @@
 <template>
     <div class="min-h-fit py-4 flex flex-col sm:justify-center items-center sm:pt-0">
         <div>
-            <slot name="logo" />
+            <slot name="logo"/>
         </div>
-
-        <div class="w-full sm:max-w-md px-6 overflow-hidden sm:rounded-lg">
-            <slot />
+        <Alert/>
+        <div class="w-full sm:max-w-lg px-6 overflow-hidden sm:rounded-lg">
+            <slot/>
         </div>
     </div>
 </template>
+<script>
+import Alert from "../Shared/Components/Alert";
+
+export default {
+    components: {Alert}
+}
+</script>
