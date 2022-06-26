2fec0d83ab7e33b39b7203165302938406fedd6d
Ali Gulzar <ali.gulzar729@gmail.com>
2022-06-26 15:10:31 +0430

    Fix seeder execution order
    This caused the UsersTableSeeder to fail because of state_id constraint to State

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,15 +15,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call(CountriesTableSeeder::class);
+        $this->call(StatesTableSeeder::class);
+        $this->call(DistrictsTableSeeder::class);
+        $this->call(CurrenciesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
         $this->call(RolesTableSeeder::class);
         $this->call(PermissionsTableSeeder::class);
         $this->call(RoleHasPermissionsTableSeeder::class);
         $this->call(ModelHasRolesTableSeeder::class);
-        $this->call(CountriesTableSeeder::class);
-        $this->call(StatesTableSeeder::class);
-        $this->call(DistrictsTableSeeder::class);
-        $this->call(CurrenciesTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
         $this->call(AttributesTableSeeder::class);
         $this->call(AttributeValuesTableSeeder::class);
