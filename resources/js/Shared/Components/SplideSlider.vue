44cdf4c7732185133cdf19a6612baa7c24f1528f
Ali Gulzar <ali.gulzar729@gmail.com>
2022-07-16 15:56:05 +0430

    Only show controls when images are more than 1

--- resources/js/Shared/Components/SplideSlider.vue
+++ resources/js/Shared/Components/SplideSlider.vue
@@ -13,13 +13,14 @@ const options = {
     gap: '1rem',
     paginationDirection: 'rtl',
     direction: 'rtl',
-    rewind: props.images.length > 0,
-    autoplay: props.images.length > 0,
-    type: props.images.length > 0 ? 'loop' : 'slide',
-    drag: props.images.length > 0,
-    keyboard: props.images.length > 0,
-    paginationKeyboard: props.images.length > 0,
-    arrows: props.images.length > 0,
+    rewind: props.images.length > 1,
+    autoplay: props.images.length > 1,
+    type: props.images.length > 1 ? 'loop' : 'slide',
+    drag: props.images.length > 1,
+    keyboard: props.images.length > 1,
+    pagination: props.images.length > 1,
+    paginationKeyboard: props.images.length > 1,
+    arrows: props.images.length > 1,
     lazyLoad: 'nearby',
 }
 </script>
@@ -27,7 +28,13 @@ const options = {
 <template>
     <Splide :options="options" :has-track="true" aria-label="تصاویر آگهی"
             class="w-full max-h-[34rem] order-first lg:order-last pt-4 lg:py-0 mb-12 md:mb-0">
-        <SplideSlide v-if="images.length > 0"
+        <SplideSlide v-if="images.length === 1"
+                     :id="images[0].uuid">
+            <img :src="images[0].url" :data-splide-lazy="images[0].url"
+                 :alt="modelData.title + '_image_' + images[0].uuid"
+                 class="rounded-lg w-full object-cover" loading="lazy"/>
+        </SplideSlide>
+        <SplideSlide v-if="images.length > 1"
                      v-for="image in images"
                      :key="image.uuid"
                      :id="image.uuid">
