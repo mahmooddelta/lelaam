21808dad30494f50d85813f3e91bb2e9ef37c10d
Ali Gulzar <ali.gulzar729@gmail.com>
2022-07-19 09:13:49 +0430

    Implement export bulk action

--- app/Filament/Resources/AdResource.php
+++ app/Filament/Resources/AdResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources;
 
+use AlperenErsoy\FilamentExport\Actions\FilamentExportBulkAction;
 use App\Filament\Resources\AdResource\Pages;
 use App\Filament\Resources\AdResource\RelationManagers;
 use App\Models\Ad;
@@ -32,6 +33,7 @@ use function __;
 use function count;
 use function is_null;
 use function now;
+use function str;
 
 class AdResource extends Resource
 {
@@ -357,6 +359,18 @@ class AdResource extends Resource
                                 fn(Builder $query, $date): Builder => $query->whereDate('updated_at', '<=', $date),
                             );
                     }),
+            ])->bulkActions([
+                FilamentExportBulkAction::make('export')
+                    ->label(__('general.export.bulk_action_button_label'))
+                    ->fileName(str(self::$model)->after("App\Models\\"))
+                    ->fileNameFieldLabel(__('general.export.file_name_field_label')) // Label for file name input
+                    ->formatFieldLabel(__('general.export.format_field_label')) // Label for format input
+                    ->pageOrientationFieldLabel(__('general.export.page_orientation_field_label')) // Label for page orientation input
+                    ->filterColumnsFieldLabel(__('general.export.filters_column_field_label')) // Label for filter columns input
+                    ->additionalColumnsFieldLabel(__('general.export.additional_columns_field_label')) // Label for additional columns input
+                    ->additionalColumnsTitleFieldLabel(__('general.export.additional_columns_title_field_label')) // Label for additional columns' title input
+                    ->additionalColumnsDefaultValueFieldLabel(__('general.export.additional_columns_default_value_field_label')) // Label for additional columns' default value input
+                    ->additionalColumnsAddButtonLabel(__('general.export.additional_columns_add_button_label')) // Label for additional columns' add button,
             ]);
     }
 
