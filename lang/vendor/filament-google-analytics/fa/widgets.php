07ed378d6a7742627025a39496f23b187b43cd5c
Ali Gulzar <ali.gulzar729@gmail.com>
2022-07-19 14:35:32 +0430

    Add Google Analytics Dashboard

---
+++ lang/vendor/filament-google-analytics/fa/widgets.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+    /**
+     * Dashboard Page
+     */
+    'navigation_label' => 'داشبورد تجزیه و تحلیل ترافیک گوگل',
+    'title' => 'داشبورد تجزیه و تحلیل ترافیک گوگل',
+
+    /**
+     * Widget Heading/Labels
+     */
+    'page_views' => 'بازدید صفحات',
+    'visitors' => 'کاربران منحصر بفرد',
+    'sessions' => 'جلسات',
+    'sessions_duration' => 'میانگین مدت زمان جلسات',
+    'sessions_by_country' => 'جلسات بر اساس کشور',
+    'sessions_by_device' => 'جلسات بر اساس دستگاه',
+    'top_referrers' => 'ارجاع کننده گان برتر',
+    'most_visited_pages' => 'صفحات با بیشترین بازدید',
+    'one_day_active_users' => 'کاربران فعال در یک روز',
+    'seven_day_active_users' => 'کاربران فعال در 7 روز',
+    'fourteen_day_active_users' => 'کاربران فعال در 14 روز',
+    'twenty_eight_day_active_users' => 'کاربران فعال در 28 روز',
+
+    /**
+     * Filter Labels
+     */
+    'T' => 'امروز',
+    'Y' => 'دیروز',
+    'TW' => 'این هفته',
+    'LW' => 'هفته گذشته',
+    'LM' => 'ماه قبل',
+    'TM' => 'این ماه',
+    'TY' => 'امسال',
+    'LSD' => 'هفت روز گذشته',
+    'LTD' => '30 روز گذشته',
+    'FD' => '5 روز',
+    'TD' => '10 روز',
+    'FFD' => '15 روز',
+
+    /**
+     * Trajectory Label Descriptions
+     */
+    'trending_up' => 'افزایش',
+    'trending_down' => 'کاهش',
+    'steady' => 'ثابت/استوار',
+];
