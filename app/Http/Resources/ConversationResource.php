f6523694d9698c22d4c6137042e60f7ffdfbeefb
Ali Gulzar <ali.gulzar729@gmail.com>
2022-07-20 08:54:53 +0430

    Format timestamps for humans

--- app/Http/Resources/ConversationResource.php
+++ app/Http/Resources/ConversationResource.php
@@ -19,8 +19,8 @@ class ConversationResource extends JsonResource
         return [
             'id' => $this->id,
 
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
+            'created_at' => $this->created_at->diffForHumans(),
+            'updated_at' => $this->when($this->updated_at, $this->updated_at->diffForHumans()),
             'messages_count' => $this->when($this->messages_count, $this->messages_count),
 
             'ad' => new AdResource($this->whenLoaded('ad')),
