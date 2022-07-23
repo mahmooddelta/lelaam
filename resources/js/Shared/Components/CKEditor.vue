e1b080dd63170f87aaae387ca891531d4eb7a4b2
Ali Gulzar <ali.gulzar729@gmail.com>
2022-07-23 14:37:40 +0430

    Remove heading in toolbar as it can troublesome

--- resources/js/Shared/Components/CKEditor.vue
+++ resources/js/Shared/Components/CKEditor.vue
@@ -33,7 +33,6 @@ export default {
             config: {
                 language: 'fa',
                 toolbar: [
-                    "heading", "|",
                     'bold', 'italic', '|',
                     'bulletedList', 'numberedList', '|',
                     'blockQuote', '|',
