8d9e9445a8a420a8c3f4d7f498c2f2bdf54705ad
Ali Gulzar <ali.gulzar729@gmail.com>
2022-07-24 08:21:36 +0430

    refactor: Cleanup up plugin remainders

--- vite.production.config.js
+++ vite.production.config.js
@@ -1,23 +1,22 @@
 import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import livewire from '@defstudio/vite-livewire-plugin';
 import path from "path";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.js', ssr: 'resources/js/ssr.js',
+            input: 'resources/js/app.js',
+            ssr: 'resources/js/ssr.js',
+            refresh: true,
         }), vue({
             template: {
                 transformAssetUrls: {
                     base: null, includeAbsolute: false,
                 },
             },
-        }), livewire({
-            refresh: ['resources/css/app.css'],
-            watch: ['./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php', './vendor/laravel/jetstream/**/*.blade.php', './storage/framework/views/*.php', '**/app/**/Livewire/**/*.php', './vendor/filament/**/*.blade.php',],
-        })], server: {
+        }),],
+    server: {
         cors: true, https: {
             maxVersion: 'TLSv1.2',
         },
