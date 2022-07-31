8791115b2cc3022bdad5116d922e22f2891e5505
Ali Gulzar <ali.gulzar729@gmail.com>
2022-07-31 14:20:28 +0430

    fix: User cannot send message to his own ad
    The condition was incorrectly implemented as it prevents the user from sending a reply to a chat that was initiated by another user.

--- app/Http/Controllers/Api/ChatController.php
+++ app/Http/Controllers/Api/ChatController.php
@@ -83,30 +83,24 @@ class ChatController extends Controller
     {
         $ad->load(['media'])->select(['id', 'title', 'slug', 'phone_number', 'user_id']);
         if ($ad->user_id !== 0) {
-            if ($ad->user_id !== auth('api')->id()) {
-                try {
-                    \DB::transaction(function() use ($request, $ad) {
-                        $message = Message::create(
-                            [
-                                'conversation_id' => $request->validated('conversation_id'),
-                                'sender_id' => auth('api')->id(),
-                                'receiver_id' => $ad->user_id,
-                                'body' => $request->validated('message'),
-                            ]);
-                        broadcast(new MessageSentEvent($message->conversation, $message))->toOthers();
-                    });
-                } catch (Exception $exception) {
-                    \Log::error($exception);
+            try {
+                \DB::transaction(function() use ($request, $ad) {
+                    $message = Message::create(
+                        [
+                            'conversation_id' => $request->validated('conversation_id'),
+                            'sender_id' => auth('api')->id(),
+                            'receiver_id' => $ad->user_id,
+                            'body' => $request->validated('message'),
+                        ]);
+                    broadcast(new MessageSentEvent($message->conversation, $message))->toOthers();
+                });
+            } catch (Exception $exception) {
+                \Log::error($exception);
 
-                    return \response()->json(['message' => '!مشکلی در ارسال پیام شما پیش آمده است. لطفا دوباره کوشش کنید']);
-                }
-
-                return \response()->json(['message' => '.پیام ارسال شد']);
+                return \response()->json(['message' => '!مشکلی در ارسال پیام شما پیش آمده است. لطفا دوباره کوشش کنید']);
             }
 
-            return response()->json([
-                'message' => '!چت با خودتان غیرمنطقی است و ممکن نیست',
-            ], ResponseAlias::HTTP_UNAUTHORIZED);
+            return \response()->json(['message' => '.پیام ارسال شد']);
         }
 
         return response()->json([
