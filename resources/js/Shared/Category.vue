f2f23553d30915ef583104034685d09f0a3e6ed5
Ali Gulzar <ali.gulzar729@gmail.com>
2022-08-03 10:36:57 +0430

    chore: Show only published ads

--- resources/js/Shared/Category.vue
+++ resources/js/Shared/Category.vue
@@ -9,7 +9,7 @@ defineProps({
         class="w-full card card-side bg-base-100 shadow-xl my-2 text-right border-2 border-primary-500 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-100 duration-300 hover:border-1 hover:shadow-2xl">
         <div class="card-body text-center">
             <h2 class="font-bold text-lg" v-html="category.name"></h2>
-            <p class="text-sm" v-text="`${(category.ads_count || 0)} آگهی `"></p>
+            <p class="text-sm" v-text="`${(category.ads_count || 0)} آگهی جدید `"></p>
             <div class="card-actions justify-center my-2">
                 <Link :href="route('ads', { category: category.slug })" class="btn btn-primary btn-sm">دیدن تمام آگهی ها</Link>
             </div>
