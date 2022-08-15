2275928a7d881b6232a5b9256fc7aae44b06543f
Ali Gulzar <ali.gulzar729@gmail.com>
2022-08-15 15:51:56 +0430

    chore: Add settings seeder data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -31,5 +31,6 @@ class DatabaseSeeder extends Seeder
 //        $this->call(AdSeeder::class);
         $this->call(AdsTableSeeder::class);
         $this->call(MediaTableSeeder::class);
+        $this->call(SettingsTableSeeder::class);
     }
 }
