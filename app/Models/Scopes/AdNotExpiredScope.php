247158869435affd46019594ef6f232e0c145d9f
Ali Gulzar <ali.gulzar729@gmail.com>
2022-09-06 20:44:05 +0430

    chore: Load only not sold posts

--- app/Models/Scopes/AdNotExpiredScope.php
+++ app/Models/Scopes/AdNotExpiredScope.php
@@ -13,11 +13,14 @@ class AdNotExpiredScope implements Scope
 {
     public function apply(Builder $builder, Model $model)
     {
-        if (! (Route::is('chat') || Route::is('chat.*') || Route::is('conversation.*')
+        if (!(Route::is('chat') || Route::is('chat.*') || Route::is('conversation.*')
             || \request()->is('api/*/chat') || \request()->is('api/*/conversation')
             || \request()->is('api/*/post/*/update') || \request()->is('api/*/post/*/edit')
-            || request()->is('api/*/conversations') || request()->is('post/*/edit'))) {
-            $builder->where('expires_at', '>', Carbon::parse($model->published_at ?? $model->created_at));
+            || request()->is('api/*/conversations') || request()->is('post/*/edit')
+            || request()->is('api/*/post/*/sold') || request()->is('post/*/sold'))
+        ) {
+            $builder->where('expires_at', '>', Carbon::parse($model->published_at ?? $model->created_at))
+                ->where('is_sold', false);
         }
     }
 }
