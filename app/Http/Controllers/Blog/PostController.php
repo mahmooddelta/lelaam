958f41eba8d43c5a58320d12ca52b9d9d5291270
Ali Gulzar <ali.gulzar729@gmail.com>
2022-09-10 16:15:43 +0430

    feat: Ability to filter blog posts by tag

--- app/Http/Controllers/Blog/PostController.php
+++ app/Http/Controllers/Blog/PostController.php
@@ -19,7 +19,7 @@ class PostController extends Controller
                 [
                     'sortBy' => Rule::in(['newest', 'oldest']),
                     'category_id' => Rule::exists('blog_categories', 'id'),
-                    'perPage' => 'integer|min:0|max:100',
+                    'perPage' => 'integer|min:5|max:100',
                 ])->validate();
         }
 
@@ -36,9 +36,12 @@ class PostController extends Controller
             'media',
             'tags',
         ])
+            ->when(request()->has('tag') && request()->filled('tag'), fn(Builder $builder) => $builder->whereHas('tags', fn($query) => $query->containing(request()->input('tag'))))
             ->when(request()?->filled('category_id'), fn(Builder $builder) => $builder->where('blog_category_id', request('category_id')))
             ->when(request()?->has('sortBy'), fn(Builder $builder) => $builder->orderBy($sortBy, $order))
-            ->when(! request()?->has('sortBy'), fn(Builder $builder) => $builder->orderBy('published_at', 'desc'))
-            ->paginate(request('perPage', 10)));
+            ->when(!request()?->has('sortBy'), fn(Builder $builder) => $builder->orderBy('published_at', 'desc'))
+            ->paginate(request('perPage', 6))
+            ->withQueryString()
+        );
     }
 }
