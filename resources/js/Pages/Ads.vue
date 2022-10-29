dd5a683bee254db799506bd753a9739767a2eb9c
Ali Gulzar <ali.gulzar729@gmail.com>
2022-10-29 17:55:26 +0430

    chore: Show empty ads text on ads page

--- resources/js/Pages/Ads.vue
+++ resources/js/Pages/Ads.vue
@@ -28,11 +28,16 @@ const {filters, isLoading} = useFilters({
     <section class="w-full">
         <Filters :categories="categories" :states="states" :districts="districts" :filters="filters" v-model="filters"/>
     </section>
-    <section class="w-full text-center my-4">
+    <section class="w-full text-center my-4" v-if="ads.data && ads.data.length > 0">
         <section class="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xs:grid-cols-4 gap-4 px-4">
             <Ad v-for="ad in ads.data" :key="ad.id" :ad="ad"/>
         </section>
         <div class="divider"></div>
         <Pagination :links="ads.meta.links"/>
     </section>
+    <div v-else class="py-12">
+        <h1 class="text-4xl text-center font-bold">
+            متاسفیم. هیچ آگهی با فیلتر شما پیدا نشد!
+        </h1>
+    </div>
 </template>
